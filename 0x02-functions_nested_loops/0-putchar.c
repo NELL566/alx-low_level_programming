916d3c42c51789d650a1ab76963789da1942407b
NELL566 <odemeroheaven@gmail.com>
2022-03-15 23:36:23 +0100

    Print a new file in lowercase

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,26 @@
  */
 int main(void)
 {
-	char *sh = "_putcher";
+	char *sh = "_putche#include "_putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char *sh = "_putcher";
+
+        while (*sh)
+        {
+                _putchar(*sh);
+                sh++;
+        }
+        _putchar('\n');
+
+        return (0);
+}
+r";
 
 	while (*sh)
 	{
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,30 @@
-#include "_putchar.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-        char *sh = "_putcher";
+#define MAIN_H
 
-        while (*sh)
-        {
-                _putchar(*sh);
-                sh++;
-        }
-        _putchar('\n');
+/* _putchar.c */
+int _putchar(char);
 
-        return (0);
-}
+/*10 x alphabet */
+void print_alphabet(void);
 
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int n);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
