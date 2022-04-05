e2bb0de0b74444a8fb6b24ec59aeea35bae24f86
NELL566 <odemeroheaven@gmail.com>
2022-04-06 00:44:10 +0100

    Write a program that prints the number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/**/**
- * @brief 
- * 
- */
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+#include "main.h"
+
+/**
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
